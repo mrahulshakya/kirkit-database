6dabfc2d37caafe5a75c14182a7420c7d53b97a3
Rahul  Shakya <rshakya@legalzoom.com>
2018-05-29 14:44:18 +0530

    master data update

--- scorer/master_data.sql
+++ scorer/master_data.sql
@@ -8,32 +8,33 @@
 --Obstructing the field
 --Timed out
 --Hit the ball twice
-INSERT INTO [dbo].[WicketType] (Detail,  DtCreated,    
+INSERT INTO [dbo].[WicketType] (WicketTypeId,Detail,  DtCreated,    
     DtUpdated,
     IsActive)
-VALUES ('Bowled', GETDATE(), GETDATE(), 1),
-       ('Caught', GETDATE(), GETDATE(), 1),
-       ('Leg Before Wicket', GETDATE(), GETDATE(), 1),
-       ('Run Out', GETDATE(), GETDATE(), 1),
-       ('Stumped', GETDATE(), GETDATE(), 1),
-       ('Hit Wicket', GETDATE(), GETDATE(), 1),
-       ('Handled the ball', GETDATE(), GETDATE(), 1),
-       ('Obstructing the field', GETDATE(), GETDATE(), 1),
-       ('Hit the ball  twice', GETDATE(), GETDATE(), 1);
+VALUES (1,'Bowled', GETDATE(), GETDATE(), 1),
+       (2,'Caught', GETDATE(), GETDATE(), 1),
+       (3,'Leg Before Wicket', GETDATE(), GETDATE(), 1),
+       (4,'Run Out', GETDATE(), GETDATE(), 1),
+       (5,'Stumped', GETDATE(), GETDATE(), 1),
+       (6,'Hit Wicket', GETDATE(), GETDATE(), 1),
+       (7,'Handled the ball', GETDATE(), GETDATE(), 1),
+       (8,'Obstructing the field', GETDATE(), GETDATE(), 1),
+       (9,'Hit the ball  twice', GETDATE(), GETDATE(), 1),
+       (10,'Timed Out', GETDATE(), GETDATE(), 1);
 
 --Normal
 --No Ball
 --Wide Ball
 --Dead Ball
 --First Bounce
-INSERT INTO [dbo].[BallType] (Detail,  DtCreated,    
+INSERT INTO [dbo].[BallType] (BallTypeId,Detail,  DtCreated,    
     DtUpdated,
     IsActive,IsLegal, ExtraRun)
-VALUES ('Normal', GETDATE(), GETDATE(), 1, 1, 0),
-       ('No ball', GETDATE(), GETDATE(), 1, 0, 1),
-       ('Wide ball', GETDATE(), GETDATE(), 1,0,1),
-       ('Dead Ball', GETDATE(), GETDATE(), 1,0,0),
-       ('First Bounce', GETDATE(), GETDATE(), 1,1,0);  
+VALUES (1,'Normal', GETDATE(), GETDATE(), 1, 1, 0),
+       (2,'No ball', GETDATE(), GETDATE(), 1, 0, 1),
+       (3,'Wide ball', GETDATE(), GETDATE(), 1,0,1),
+       (4,'Dead Ball', GETDATE(), GETDATE(), 1,0,0),
+       (5,'First Bounce', GETDATE(), GETDATE(), 1,1,0);  
 
 -- Normal  Run 
 -- Four 
@@ -41,15 +42,15 @@ VALUES ('Normal', GETDATE(), GETDATE(), 1, 1, 0),
 -- Byes 
 -- Leg Byes 
 -- Over throw 
-INSERT INTO [dbo].[RunType] (Detail,  DtCreated,    
+INSERT INTO [dbo].[RunType] (RunTypeId,Detail,  DtCreated,    
     DtUpdated,
     IsActive)
-VALUES ('Normal', GETDATE(), GETDATE(), 1),
-       ('Four', GETDATE(), GETDATE(), 1),
-       ('Six', GETDATE(), GETDATE(), 1),
-       ('Leg Byes', GETDATE(), GETDATE(), 1),
-       ('Byes', GETDATE(), GETDATE(), 1),
-       ('Over throw', GETDATE(), GETDATE(), 1),
+VALUES (1,'Normal', GETDATE(), GETDATE(), 1),
+       (2,'Four', GETDATE(), GETDATE(), 1),
+       (3,'Six', GETDATE(), GETDATE(), 1),
+       (4,'Leg Byes', GETDATE(), GETDATE(), 1),
+       (5,'Byes', GETDATE(), GETDATE(), 1),
+       (6,'Over throw', GETDATE(), GETDATE(), 1);
    
-
+ 
 
